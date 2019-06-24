433c25323eebdc418780446392f4281f49259069
emmydee47 <emmydee47@gmail.com>
2019-06-24 05:50:58 +0100

    Added the weather Component
    Created weather component that will persist data across application

---
+++ component/weather.js
@@ -0,0 +1,20 @@
+class weather
+{
+    constructor() {
+        this.city_name = "";
+        this.temperature = "";
+        this.maximum_temperature = "";
+        this.minimum_temperature = "";
+        this.weather_icon = "";
+    }
+    
+    // constructor(weather_details) {
+    //     this.city_name = weather_details.city_name;
+    //     this.temperature = weather_details.temperature;
+    //     this.maximum_temperature = weather_details.maximum_temperature;
+    //     this.minimum_temperature = weather_details.minimum_temperature;
+    //     this.weather_icon = weather_details.weather_icon
+    // }
+}
+module.exports = weather;
+
